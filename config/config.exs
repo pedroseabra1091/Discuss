4574142242693e1b1b01aadfb3425038a8061739
pedroseabra1091 <pedroseabra1091@gmail.com>
2018-11-25 18:49:05 +0000

    Github OAuth

--- config/config.exs
+++ config/config.exs
@@ -32,6 +32,4 @@ config :ueberauth, Ueberauth,
     github: { Ueberauth.Strategy.Github, [default_scope: "user,public_repo"] }
   ]
 
-config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-  client_id: System.get_env("GITHUB_CLIENT_ID"),
-  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
+import_config "github_oauth.exs"
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -23,6 +24,7 @@ defmodule Discuss.Router do
     pipe_through :browser
 
     # wildcard to later include other uberauth providers
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
   end
